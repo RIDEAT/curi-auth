d91c4e605e3e647122907f5c5c47ddbfd24928ae
POPORION <black4roach@gmail.com>
2023-08-21 15:16:39 +0900

    modify token table name

--- src/main/java/com/auth/curi/auth/repository/entity/Token.java
+++ src/main/java/com/auth/curi/auth/repository/entity/Token.java
@@ -5,16 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
+@Table(name = "jwt_token")
 public class Token {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
